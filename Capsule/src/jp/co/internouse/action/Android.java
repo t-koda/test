73f3cbc32485f5b40d7f4fb3c3773690039007a3
t-koda <kdtmhr-1208@ezweb.ne.jp>
2018-11-05 21:30:32 +0900

    11/5

---
+++ Capsule/src/jp/co/internouse/action/Android.java
@@ -0,0 +1,19 @@
+package jp.co.internouse.action;
+
+public class Android {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		SmartPhone android = new SmartPhone();
+		android.play();
+		android.stop();
+		android.next();
+		android.back();
+		android.call();
+		android.mail();
+		android.photo();
+		android.internet();
+	}
+
+}
